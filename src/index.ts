c1d02e582325f57ac933bac601f3c24d91b79cf1
Devin Warrick <dev.warr@gmail.com>
2019-10-26 14:09:50 -0500

    Import dotenv for dynamic port ability

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 // Initial imports
 import cors from "cors";
+import "dotenv/config";
 import express from "express";
 import helmet from "helmet";
 
@@ -16,5 +17,5 @@ server.get("/", (req, res) => {
     res.send("Well, here we are, huh..?");
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n***Listening on Port ${port}***\n`));
