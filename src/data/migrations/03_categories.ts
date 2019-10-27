f8617cd9ca8f74d3e1473a7656db08293701fc2c
Devin Warrick <dev.warr@gmail.com>
2019-10-26 21:51:13 -0500

    Categories migrations and seeds created. Todos edited to now have category ids

---
+++ src/data/migrations/03_categories.ts
@@ -0,0 +1,14 @@
+
+export const up = (knex: any) => knex.schema.createTable("categories", (category: any) => {
+    category.increments();
+    category.integer("list_id")
+        .references("id")
+        .inTable("lists")
+        .notNullable();
+    category.string("name")
+        .notNullable();
+    category.string("color_primary");
+    category.string("color_secondary");
+});
+
+export const down = (knex: any) => knex.schema.dropTableIfExists("categories");
