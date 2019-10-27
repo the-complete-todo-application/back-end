8c96c6457a611902cdebe928e7f7bf2e0de0b9cb
Devin Warrick <dev.warr@gmail.com>
2019-10-26 19:40:16 -0500

    List migrations and seedData coded

---
+++ src/data/seeds/lists.ts
@@ -0,0 +1,15 @@
+import IList from "../../models/List";
+
+const seedData: IList[] = [
+    {name: "Todos"},
+    {name: "Groceries"},
+    {name: "Chores", color_primary: "EEEEEE", color_secondary: "222222"},
+    {name: "Hitman List"},
+    {name: "Games to speedrun"},
+    {name: "Todo Application"}
+];
+
+export const seed = (knex: any) => {
+    return knex("lists").truncate()
+        .then(() => knex("lists").insert(seedData));
+};
