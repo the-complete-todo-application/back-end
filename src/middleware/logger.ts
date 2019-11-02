e0470fcb4f4de9ba4375e460a0fc2b995f932eb3
Devin Warrick <dev.warr@gmail.com>
2019-11-01 22:43:40 -0500

    Adding in error handling

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from "express";
 export default function(req: Request, res: Response, next: NextFunction) {
     console.log(
         `[${new Date().toISOString()}] ` +
-        `\x1b[32m${req.method.toUpperCase()} ` +
+        `\x1b[32m${req.method.toUpperCase()}\x1b[0m ` +
         `${req.originalUrl} ` +
         ``
     );
