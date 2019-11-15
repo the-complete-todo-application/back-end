80fa5c68b73dd4f14297c77926ffef9589f7a543
Devin Warrick <dev.warr@gmail.com>
2019-11-15 03:41:03 -0600

    Added cross-env to set environment to testing; added test to check for environment

--- tests/server.test.ts
+++ tests/server.test.ts
@@ -8,6 +8,10 @@ jest.mock("../src/middleware/errorHandler", () => {
     });
 });
 
+test("We're in the testing environment", () => {
+    expect(process.env.DB_ENV).toBe("testing");
+})
+
 describe("Get \"/\"", () => {
     test("Receive 200 with text", async () => {
         const expectedString = "Well, here we are, huh..?";
